8de537a6715ca49ca7e2401909cf590c336de65d
kinetic <20599331+ananjiahhao@users.noreply.github.com>
2014-07-22 17:00:00 -0700

    build: iam

--- src/grantdecay/entitlement.py
+++ src/grantdecay/entitlement.py
@@ -11,3 +11,10 @@ A ``role`` record declares that a role confers one or more permissions. A role
 may appear on more than one ``role`` line, in which case the permission sets are
 unioned. A ``grant`` record assigns one or more roles to a principal.
 
+Expansion turns the two record kinds into the effective permission set for each
+principal: the union of the permissions of every role granted to that principal.
+A grant that names a role never declared by any ``role`` record is an error,
+because it means the export cannot be reasoned about honestly.
+
+The parser is strict: it reports the file, line number, and reason for every
+malformed record rather than guessing.
