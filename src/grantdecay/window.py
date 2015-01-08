582acef65180b347fd1c5ba0e41afb902300eb78
kinetic <20599331+ananjiahhao@users.noreply.github.com>
2015-01-08 11:52:00 -0700

    style: clean up cli

--- src/grantdecay/window.py
+++ src/grantdecay/window.py
@@ -4,3 +4,9 @@ Absence of use inside a window is not proof that a permission is unneeded. A
 permission might be exercised only at quarter close, during an incident, or on a
 yearly audit. The honest guard against over-claiming is a minimum window: below
 it, grantdecay refuses to label anything as unused and says so plainly.
+
+This module defines the Window value type, its inclusive length in days, and the
+default minimum of 30 days. The default is a policy choice, not a measurement: it
+is documented as such and can be overridden on the command line.
+"""
+
