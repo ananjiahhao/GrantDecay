acbfcc53b90e89e660bf1e76d4548b5ff4b5a1e5
kinetic <20599331+ananjiahhao@users.noreply.github.com>
2017-03-13 15:33:00 -0700

    test: cover security

--- src/grantdecay/cli.py
+++ src/grantdecay/cli.py
@@ -12,3 +12,15 @@ access log export. They exit 1 when findings are present, 0 when clean.
 
 Exit codes: 0 clean, 1 findings present, 2 usage error. argparse exits with 2 on
 argument errors, which matches the standard.
+"""
+
+from __future__ import annotations
+
+import argparse
+import sys
+
+from . import __version__
+from .accesslog import AccessLogError, parse_access_log
+from .decay import analyze, build_surface
+from .entitlement import EntitlementError, parse_entitlements
+from .report import render_report, render_surface, render_unused
