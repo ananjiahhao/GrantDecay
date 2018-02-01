45a0ce4009cdc01e1a50cb7e91617be9074fd828
kinetic <20599331+ananjiahhao@users.noreply.github.com>
2018-02-01 11:00:00 -0700

    fix: correct access-review

--- tests/test_grantdecay.py
+++ tests/test_grantdecay.py
@@ -20,3 +20,18 @@ from grantdecay.decay import (
 )
 from grantdecay.entitlement import EntitlementError, parse_entitlements
 from grantdecay.window import DEFAULT_MIN_DAYS, Window, WindowError, is_conclusive
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+SAMPLES = os.path.join(os.path.dirname(HERE), "samples")
+ENT_PATH = os.path.join(SAMPLES, "entitlements.txt")
+LOG_PATH = os.path.join(SAMPLES, "accesslog.txt")
+
+
+def _read(path: str) -> str:
+    with open(path, "r", encoding="utf-8") as handle:
+        return handle.read()
+
+
+class WindowTests(unittest.TestCase):
+    def test_length_is_inclusive(self):
+        w = Window(date(2026, 6, 1), date(2026, 6, 1))
